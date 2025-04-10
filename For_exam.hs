05dacb5b73ad087dbecd9b7b809003570bb27522
EcenurSarıkaya <163454341+PessimistA@users.noreply.github.com>
2025-04-10 18:41:30 +0300

    Update For_exam.hs

--- For_exam.hs
+++ For_exam.hs
@@ -11,6 +11,12 @@ factorial n
   | n == 0 = 1
   | otherwise = n * factorial (n - 1)
 
+fibonacci ::( Num a,Eq a )=> a -> a 
+fibonacci n | n==0 =0
+	    | n==1 =1
+	    | otherwise= fibonacci(n-1)+ fibonacci (n-2)
+
+
 takeask :: [a] -> Int -> a
 takeask (x : xs) n
   | n == 0 = x
@@ -21,9 +27,40 @@ takelist (x : xs) n
   | n == 0 = []
   | otherwise = [x] ++ takelist xs (n - 1)
 
+take1 ::(Ord a) =>[a]->Integer->[a]//takelist başka bir versiyon
+take1 (x:xs) n  | n>0= [x] ++ take1 xs (n-1)
+		| otherwise= []
+
+
+rev :: [a] -> [a]
+rev [] = []
+rev (x:xs) = rev xs ++ [x]
+
 len :: [a] -> Int
 len [] = 0
 len (x : xs) = 1 + len xs
 
-sums :: (Show a, Show b, Show c) => a -> b -> c -> String
+sums ::Num a => [a]-> a//tüm sayıları toplar dizideki
+sums [] = 0
+sums (x:xs)= x+ sums xs
+
+sums :: (Show a, Show b, Show c) => a -> b -> c -> String//string toplaması
 sums x y z = show x ++ show y ++ show z
+
+find :: (Eq a)=> [a]-> a -> Integer
+find (x:xs) y | x==y = 0
+	      | otherwise=1+ find xs y
+
+max1 :: Ord a => [a] -> a
+max1 [x]= x
+max1 (x:xs)  | x>= maxtail=x
+	     |otherwise= maxtail
+	     where maxtail= max1 xs
+
+double2 ::Num a=> [a] -> [a]
+double2 [] = []
+double2 (x:xs)= [x*2] ++ double2 xs 
+
+topla ::Int -> Int
+topla n | n>0 = n+ topla (n-1)
+	| otherwise= 0
