5c9bea3d724386482d0dc49688bd9ce39b12dc0d
EcenurSarıkaya <163454341+PessimistA@users.noreply.github.com>
2025-04-10 20:22:06 +0300

    Update For_exam.hs

--- For_exam.hs
+++ For_exam.hs
@@ -64,3 +64,34 @@ double2 (x:xs)= [x*2] ++ double2 xs
 topla ::Int -> Int
 topla n | n>0 = n+ topla (n-1)
 	| otherwise= 0
+add :: (Int, Int) -> Int
+add (x, y) = x + y
+
+makelist :: Int -> [Int]
+makelist x = [0 .. x]
+
+drop2 :: (Eq a) => Int -> [a] -> [a]
+drop2 n (x : xs)
+  | n == 1 = xs
+  | otherwise = drop2 (n - 1) xs
+
+zip2 :: [a] -> [b] -> [(a, b)]
+zip2 [] [] = []
+zip2 (x : xs) (y : ys) = (x, y) : zip2 xs ys
+
+safetail :: [a] -> [a]
+safetail [] = error "Empty list"
+safetail (x : xs) = xs
+
+safetail2 :: [a] -> [a]
+safetail2 xs | null xs = error "Empty list" | otherwise = tail xs
+
+condition :: Bool -> Bool -> Bool
+condition x y
+  | x == True && y == True = True
+  | otherwise = False
+
+condition2 :: Bool -> Bool -> Bool
+condition2 x y
+  | x == True && y == y = y
+  | x == False && y == y = False
