4f70d25be85882b826f348d228bb237bf03ec8d3
EcenurSarıkaya <163454341+PessimistA@users.noreply.github.com>
2025-04-24 00:52:58 +0300

    Update function_writing.hs

--- function_writing.hs
+++ function_writing.hs
@@ -84,3 +84,11 @@ take22 (x:xs) n j |j == n 	=[]
 printN :: Int -> String
 printN 0 = ""
 printN n = printN (n - 1) ++ show n ++ "\n "
+
+//where usage
+calculateavarage :: (Num a, Fractional a, Ord a) => a -> a -> String
+calculateavarage n m
+  | s > 8 = "Student is successful"
+  | otherwise = "Student is not successful"
+  where
+    s = n * 0.3 + m * 0.7
